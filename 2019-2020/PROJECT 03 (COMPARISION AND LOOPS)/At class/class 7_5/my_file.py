18a016519ec321f92b3980cb2922278ea3cec460
pm72 <pm72github@yahoo.com>
2020-01-30 13:16:23 +0400

    pm72

--- 2019-2020/PROJECT 03 (COMPARISION AND LOOPS)/At class/class 7_5/my_file.py
+++ 2019-2020/PROJECT 03 (COMPARISION AND LOOPS)/At class/class 7_5/my_file.py
@@ -1,13 +1,5 @@
-''' My name is Paata mamporia.
-    I was born in Tbilisi, Georgia in 1972 28th of october.
-    I'am marrid and have two chilndrens...
-'''
-
 name = "Paata Mamporia"
 age = 47
-addr = "Georgia, Tbilisi, Gldani 7-11-4-93"
-marrid = True
 
 print("Name:", name)
 print("Age:", age)
-print("Address:", addr)
